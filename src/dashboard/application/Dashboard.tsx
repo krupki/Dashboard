520ab8c76e8578d9caf6089458ef1d23c3dfede0
krupki <kschwarzm@gmail.com>
2026-02-19 22:44:58 +0100

    feat: update Header component layout and add user login functionality

--- src/dashboard/application/Dashboard.tsx
+++ src/dashboard/application/Dashboard.tsx
@@ -2,8 +2,8 @@ import { Header } from "../components/Header";
 
 export const Dashboard = () => {
   return (
-    <>
+    <div className="flex h-screen w-full">
       <Header />
-    </>
+    </div>
   );
 };
--- src/dashboard/components/Header.tsx
+++ src/dashboard/components/Header.tsx
@@ -1,7 +1,28 @@
+import { LogIn, SearchIcon } from "lucide-react";
+
 export const Header = () => {
   return (
-    <>
-      <header>Dashboard</header>
-    </>
+    <div className="sticky top-0 z-10 h-10 w-full border-b border-zinc-700 px-2 flex items-center justify-between">
+      {/* left side */}
+      <div className="flex flex-col">
+        <h2 className="shrink-0">Dashboard</h2>
+        <span className="text-xs text-zinc-600">Overview</span>
+      </div>
+      {/* center area / search bar*/}
+      <div className="flex p-1 gap-1 border-2 border-zinc-600 rounded-sm">
+        <SearchIcon className="h-4 w-4" />
+        <input type="text" placeholder="search..." className="text-xs"></input>
+      </div>
+      {/* right side / button login and user profile*/}
+      <div className="flex">
+        <button className="flex border-2 border-zinc-600 bg-yellow-200 rounded-2xl p-1">
+          <LogIn className="shrink-0 h-4 w-4" />
+          <div className="text-sm">Login</div>
+        </button>
+        <div className="flex rounded-full h-8 w-8">
+          <img src="/images/Smiley.png" alt="User" />
+        </div>
+      </div>
+    </div>
   );
 };
